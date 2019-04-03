f71fa6c845c427ca6145353e2263a8cd6dca8a61
Ben Brammer <benmatthewjdbrammer@gmail.com>
2019-04-03 12:19:12 +0100

    commit

---
+++ eaterEggHunt/locationManager.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+import MapKit
+
+class CustomAnnotation: NSObject, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var locationLabel: String?
+//    var filmLabel: String?
+    var locationDescription: String?
+//    var imageName: String?
+    
+    var color: UIColor!
+    
+    var title: String? {
+        return locationLabel
+    }
+    
+    //All variables kept here for the Location View Controller
+    
+    init(locationLabel: String, coordinate: CLLocationCoordinate2D, locationDescription: String) {
+        self.locationLabel = locationLabel
+        self.coordinate = coordinate
+        self.locationDescription = locationDescription
+//        self.filmLabel = filmLabel
+    }
+    
+}
