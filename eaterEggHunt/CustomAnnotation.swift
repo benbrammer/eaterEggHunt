b4c63d046d3986e8e57452667c99c242e49c1fa7
Charlotte <charlottebancroft@hotmail.co.uk>
2019-05-05 02:06:02 +0100

    Huge Changes
    Website error has been fixed: All Website links now added, "Egg Image" now displays an egg (individual eggs can be set), Text box removed and changed to label so users cant edit text, minor layout changes,

--- eaterEggHunt/CustomAnnotation.swift
+++ eaterEggHunt/CustomAnnotation.swift
@@ -6,10 +6,11 @@ class CustomAnnotation: NSObject, MKAnnotation {
     
     var coordinate: CLLocationCoordinate2D
     var locationLabel: String?
+    var locationImage: UIImage?
     var locationDescription: String?
+    var eggType: String?
+    var eggs: UIImage?
     var imageName: String?
-    var longitude: String?
-    var latitude: String?
     var color: UIColor!
     var annotationView: String?
     var image: UIImage!
@@ -20,12 +21,12 @@ class CustomAnnotation: NSObject, MKAnnotation {
     
     //All variables kept here for the Location View Controller & Custom Annotation
     
-    init(locationLabel: String, coordinate: CLLocationCoordinate2D, locationDescription: String, imageName: String, ticketsButton: String) {
+    init(locationLabel: String, coordinate: CLLocationCoordinate2D, locationDescription: String, imageName: String, ticketsButton: String, eggType: String) {
         self.locationLabel = locationLabel
         self.coordinate = coordinate
         self.locationDescription = locationDescription
         self.ticketsButton = ticketsButton
         self.imageName = imageName
-        
+        self.eggType = eggType
     }
 }
--- eaterEggHunt/SecondViewController.swift
+++ eaterEggHunt/SecondViewController.swift
@@ -8,89 +8,79 @@ class SecondViewController: UIViewController, UINavigationControllerDelegate, UI
     var annoations: CustomAnnotation!
     var imagePicker: UIImagePickerController!
     
+    
+    @IBOutlet weak var locationDescription: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var eggImage: UIImageView!
     @IBOutlet weak var locationImage: UIImageView!
-    @IBOutlet weak var locationDescription: UITextView!
-    @IBOutlet weak var linkWebiste: UIButton!
+    
+   // @IBOutlet weak var linkWebiste: UIButton!
     
 
 override func viewDidLoad() {
     super.viewDidLoad()
+    //    print(annotation.locationLabel!)
+    //    locationInfo.numberOfLines = 0
+    //    //If you set the number of lines to 0, it will allow for as many lines as needed - a text box would probably be better instead of using a label - but serves the same function for this.
+   locationLabel.text = annoations.locationLabel!
+    locationImage.image = UIImage(named: annoations.locationLabel!)
+    ////    filmLabel.text = annotation.filmLabel
+    locationDescription.text = annoations.locationDescription!
+    eggImage.image = UIImage(named: annoations.eggType!)
+    
+    //}
+    //
+    //
+
+    
 }
 
-
-@IBAction func linkWebsite(_ sender: UIButton) {
-    openUrl(urlStr: ViewController.ticketsButton)
-}
-func openUrl(urlStr:String!) {
-    if let url = URL(string:urlStr) {
-        UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
-    }
+    @IBOutlet weak var imageView: UIImageView!
     
-}
-
-    
-    
-    @IBOutlet weak var takeImage: UIImageView!
-    
-    // Fuction to open camera when button is pressed
-    @IBAction func takePic(_ sender: Any) {
-        imagePicker = UIImagePickerController()
+    @IBAction func takePicture(_ sender: Any) {
+        imagePicker =  UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
     }
- 
-    @IBAction func savePhoto(_ sender: UIButton) {
-        UIImageWriteToSavedPhotosAlbum(takeImage.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
-    }
     
-    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        if let error = error {
-            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-        } else {
-            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-        }
-    }
-    
+    // The ability to confirm image and set it in Image View
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        // The info dictionary may contain multiple representations of the image. You want to use the original.
-        guard (info[.originalImage] as? UIImage) != nil else {
-            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
-        }
-        // Dismiss the picker.
-        dismiss(animated: true, completion: nil)
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageView.image = info[.originalImage] as? UIImage
+        // brings the set image foward
+        self.view.bringSubviewToFront(imageView)
     }
-}
+    
+    
+    @IBAction func websiteButton(_ sender: Any) {
+        openUrl(urlStr: annoations.ticketsButton)
+    }
+    func openUrl(urlStr:String!) {
+        if let url = URL(string:urlStr) {
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+        }
+    }
 
+    }
+
+
+    
+
+
+    
+   
 
 
         
         
         
-        
-        
 
 
 
 
 
 
-//override func viewDidLoad() {
-//    super.viewDidLoad()
-//    print(annotation.locationLabel!)
-//    locationInfo.numberOfLines = 0
-//    //If you set the number of lines to 0, it will allow for as many lines as needed - a text box would probably be better instead of using a label - but serves the same function for this.
-//    locationLabel.text = CustomAnnotation.locationLabel!
-//    imageView.image = UIImage(named: annotation.locationLabel!)
-////    filmLabel.text = annotation.filmLabel
-//    locationDescription.text = annotation.locationDescription!
-//}
-//
-//
+
 
